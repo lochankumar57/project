e305fbc14c4d8fccd13a16deeda030c8cd1c8485
lochankumar57 <125385481+lochankumar57@users.noreply.github.com>
2023-02-14 22:11:13 +0530

    Create hello.php

---
+++ hello.php
@@ -0,0 +1,18 @@
+<form method="post" action="process-form.php">
+
+  <label for="name">Name:</label>
+
+  <input type="text" name="name" id="name">
+
+  
+
+  <label for="email">Email:</label>
+
+  <input type="email" name="email" id="email">
+
+  
+
+  <input type="submit" value="Submit">
+
+</form>
+
